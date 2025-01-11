1e031b66935927aebe6d434b4c14da526069883e
GyaneshwerJha <keshavk72411@gmail.com>
2025-01-11 15:03:02 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ export default function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 5000);
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
